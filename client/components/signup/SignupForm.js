2101db8975c8b20cb66adce33a9135a6ec4a116d
Dylan Shaw <dylanshaw012@gmail.com>
2017-05-13 23:12:00 +1000

    Add server side validations

--- client/components/signup/SignupForm.js
+++ client/components/signup/SignupForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 class SignupForm extends React.Component {
   constructor(props) {
@@ -8,7 +9,9 @@ class SignupForm extends React.Component {
       username: '',
       email: '',
       password: '',
-      passwordConfirmation: ''
+      passwordConfirmation: '',
+      errors: {},
+      isLoading: false
     }
   }
 
@@ -19,16 +22,21 @@ class SignupForm extends React.Component {
   }
 
   onSubmit = (e) => {
+    this.setState({ errors: {}, isLoading: true });
     e.preventDefault();
-    this.props.userSignupRequest();
+    this.props.userSignupRequest(this.state).then(
+      () => {},
+      (err) => this.setState({ errors: err.response.data, isLoading: false})
+    );
   }
 
   render() {
+    const { errors } = this.state;
     return (
       <form onSubmit={this.onSubmit}>
         <h1>Join React Twitter</h1>
 
-        <div className="form-group">
+        <div className={classnames("form-group", { 'has-error': errors.username })}>
           <label className="control-label">Username</label>
           <input
             onChange={this.onChange}
@@ -37,8 +45,9 @@ class SignupForm extends React.Component {
             name="username"
             className="form-control"
           />
+          {errors.username && <span className="help-block">{errors.username}</span>}
         </div>
-        <div className="form-group">
+        <div className={classnames("form-group", { 'has-error': errors.email })}>
           <label className="control-label">Email</label>
           <input
             onChange={this.onChange}
@@ -47,8 +56,9 @@ class SignupForm extends React.Component {
             name="email"
             className="form-control"
           />
+          {errors.email && <span className="help-block">{errors.email}</span>}
         </div>
-        <div className="form-group">
+        <div className={classnames("form-group", { 'has-error': errors.password })}>
           <label className="control-label">Password</label>
           <input
             onChange={this.onChange}
@@ -57,8 +67,9 @@ class SignupForm extends React.Component {
             name="password"
             className="form-control"
           />
+          {errors.password && <span className="help-block">{errors.password}</span>}
         </div>
-        <div className="form-group">
+        <div className={classnames("form-group", { 'has-error': errors.passwordConfirmation })}>
           <label className="control-label">Password Confirmation</label>
           <input
             onChange={this.onChange}
@@ -67,9 +78,10 @@ class SignupForm extends React.Component {
             name="passwordConfirmation"
             className="form-control"
           />
+          {errors.passwordConfirmation && <span className="help-block">{errors.passwordConfirmation}</span>}
         </div>
         <div className="form-group">
-          <button className="btn btn-primary btn-lg">
+          <button  disabled={this.state.isLoading} className="btn btn-primary btn-lg">
             Sign Up
           </button>
         </div>
--- server/index.js
+++ server/index.js
@@ -1,18 +1,28 @@
 import express from 'express';
-const app = express();
 import path from 'path';
+import bodyParser from 'body-parser';
 
 import webpack from 'webpack';
 import webpackConfig from '../webpack.config.dev';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
-const compiler = webpack(webpackConfig);
 
+import users from './routes/users';
+
+const app = express();
+
+app.use(bodyParser.json());
+// Set users route
+app.use('/api/users', users);
+
+// Webpack configuration/hot reloading
+const compiler = webpack(webpackConfig);
 app.use(webpackDevMiddleware(compiler, {
     noInfo: true, publicPath: webpackConfig.output.publicPath
 }));
 app.use(webpackHotMiddleware(compiler));
 
+// Handle all requests with *
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 })
