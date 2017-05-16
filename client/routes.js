82f90098b6d82aefb9fda938fbd8f934206f7382
Dylan Shaw <dylanshaw012@gmail.com>
2017-05-16 22:53:50 +1000

    Added login form

--- client/routes.js
+++ client/routes.js
@@ -4,12 +4,14 @@ import { Route, Switch } from 'react-router-dom';
 import App from './components/App';
 import Greetings from './components/Greetings';
 import SignupPage from './components/signup/SignupPage';
+import LoginPage from './components/login/LoginPage';
 
 export default (
   <App>
     <Switch>
       <Route exact path="/" component={Greetings} />
       <Route path="/signup" component={SignupPage} />
+      <Route path="/login" component={LoginPage} />
     </Switch>
   </App>
 );
